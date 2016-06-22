8b24812df442650aed86625151cc0e298672380f
noby <n.fujioka@abacusmaths.info>
2016-06-23 00:47:01 +0100

    Finished Section 8.Exercise 5

--- directory.rb
+++ directory.rb
@@ -1,22 +1,43 @@
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    if name.length < 12
-      # add the student hash to the array
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-    else
-      puts "Name has to be shorter than 12 characters. This name will not be added."
-    end
-    # get another name from the user
+
+  while true
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
     name = gets.chomp
+
+    if name.empty?
+      break
+    end
+
+    if name.length >= 12
+      puts "Name has to be shorter than 12 characters. This name will not be added."
+      puts "Please enter a name under 12 characters."
+      next
+    end
+
+    hobbies = []
+    3.times do |x|
+      puts "What is the student's hobby(#{x+1})?"
+      hobby = gets.chomp
+      hobbies << hobby unless hobby.empty?
+    end
+
+    puts "Where is the student's country of birth?"
+    cOBirth = gets.chomp
+
+    puts "What is the height of the student?"
+    height = gets.chomp
+
+    puts "What is the weight of the student?"
+    weight = gets.chomp
+
+    # add the student hash to the array
+    students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: cOBirth, height: height, weight: weight  }
+    puts "Now we have #{students.count} students"
   end
   # return the array of students
   students
@@ -49,6 +70,8 @@ def print(students)
     student = students[index]
     if student[:name][0].downcase==firstLetter
       puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+      puts "  Hobbies: #{student[:hobbies].join(', ')}."
+      puts "  Country of birth: #{student[:country_of_birth]}. Height: #{student[:height]}. Weight: #{student[:weight]}."
     end
     index +=1
   end
