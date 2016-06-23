e4ac53d0d5a15e432d7c8910432cdff23c60d3ca
noby <n.fujioka@abacusmaths.info>
2016-06-23 11:25:06 +0100

    Finished Section8.exercise 7.

--- directory.rb
+++ directory.rb
@@ -1,9 +1,10 @@
 
-def input_students
-  # create an empty array
+def input_students ()
   students = []
-  # get the first name
-
+  cohorts = [:January, :February, :March,
+              :April, :May, :June,
+              :July, :August, :September,
+              :October, :November, :December]
   while true
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
@@ -17,6 +18,14 @@ def input_students
       puts "Name has to be shorter than 12 characters. This name will not be added."
       puts "Please enter a name under 12 characters."
       next
+    end
+
+    cohort = nil
+    until !cohorts.select{|month| month == cohort}.empty?
+      puts "Which cohort is the student in? (January to December)"
+      cohort = gets.chomp
+      cohort = "July" if cohort.empty?
+      cohort = cohort.gsub(/\s+/,+"_").capitalize.to_sym
     end
 
     hobbies = []
@@ -36,22 +45,23 @@ def input_students
     weight = gets.chomp
 
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: cOBirth, height: height, weight: weight  }
+    students << {name: name, cohort: cohort, hobbies: hobbies, country_of_birth: cOBirth, height: height, weight: weight  }
     puts "Now we have #{students.count} students"
+    puts
   end
   # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts " The students of Villains Academy ".center(120, "xxx")
+  puts "-------------".center(120)
 end
 
 def print(students)
-  puts "Student Search (Type 'quit' to quit)"
+  puts "Student Search (Type 'quit' to quit)".center(120)
   while true
-    puts "Please input the first letter of the students'name you want to search for:"
+    puts "Please input the first letter of the students'name you want to search for:".center(120)
     firstLetter = gets.chomp.downcase
     if firstLetter == "quit"
       puts "quitting.."
@@ -69,16 +79,18 @@ def print(students)
   until index > students.count-1
     student = students[index]
     if student[:name][0].downcase==firstLetter
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-      puts "  Hobbies: #{student[:hobbies].join(', ')}."
-      puts "  Country of birth: #{student[:country_of_birth]}. Height: #{student[:height]}. Weight: #{student[:weight]}."
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(120)
+      puts "Hobbies: #{student[:hobbies].join(', ')}.".center(120)
+      puts "Country of birth: #{student[:country_of_birth]}".center(120)
+      puts " Height: #{student[:height]} Weight: #{student[:weight]}".center(120)
     end
+    puts
     index +=1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(120)
 end
 
 students = input_students
