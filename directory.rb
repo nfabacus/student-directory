c58b2fa15ee6bdcababb076d23a1c3fc89266893
noby <n.fujioka@abacusmaths.info>
2016-06-23 19:59:36 +0100

    Finished section 9.

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 
-def input_students ()
-  students = []
+def input_students (existingList)
+  students = existingList
   cohorts = [:January, :February, :March,
               :April, :May, :June,
               :July, :August, :September,
@@ -60,10 +60,10 @@ end
 
 def print(students)
   if students.count == 0
-    puts "No list to print. Good bye!"
-    exit
+    puts "No list to print.".center(120)
+    return
   end
-  
+
   selectedStudents = []
   puts "Student Search (Type 'quit' to quit)".center(120)
   while true
@@ -118,7 +118,29 @@ def print_footer(names)
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do.
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items.
+    #2. read the input and save it into a variable.
+    selection = gets.chomp
+    #3. do what the user has asked.
+    case selection
+    when "1"
+      students = input_students(students)
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again."
+    end
+  end
+end
+
+interactive_menu
